e42718a02cb0d7aea43c9d1bcb30fe59769388b2
Michal Kowalczyk <mickowalczyk6@gmail.com>
2021-12-07 13:50:43 +0100

    add handling opinion delete

--- index.js
+++ index.js
@@ -5,14 +5,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-const opinions = [
-  {
-    text: 'pierwsza opinia, a mietek dalej niezdzielony..',
-    date: '2020-12-03',
-    author: 'zbyniek'
-  }
-]
-
+const opinions = []
 
 app.get('/', (req, res) => {
   console.log(req)
@@ -27,6 +20,17 @@ app.post('/opinions', (req, res) => {
   console.log(req.body)
   opinions.push(req.body)
   res.status(200).end()
+})
+
+app.delete('/opinions/:opinionID', (req, res) => {
+  const id = req.params.opinionID
+  const deletedOpinionID = opinions.findIndex(element => element.id === id)
+  
+  if(deletedOpinionID === -1) res.status(404).end()
+  else {
+    opinions.splice(deletedOpinionID, 1)
+    res.status(200).end()
+  }
 })
 
 app.listen(8888, () => {
