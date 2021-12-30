32ea85d02081e09914e60bdfe8ce3c90632ade70
Michal Kowalczyk <mickowalczyk6@gmail.com>
2021-12-30 19:16:45 +0100

    fix db connect

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose')
+const dburl = require('../configdb.js')
 
-mongoose.connect(`mongodb+srv://mongobagngabamamemafofofo:pRM%4034r%23%40Tt4%24XoWD@cluster0.zqmge.mongodb.net/react-movie-browser-db?retryWrites=true&w=majority`, {
+const url = process.env.DATABASE_URL || dburl
+
+mongoose.connect(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => console.log('polaczono z db')).catch(() => console.log('nie udalo sie polaczyc z db'))
