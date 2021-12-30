13ddae7d987cb1fd982f0fa2bb0a97f652b50142
Michał Kowalczyk <70285236+michakow@users.noreply.github.com>
2021-12-30 18:34:54 +0100

    update mongoose.js

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(`mongodb+srv://pRM@34r#@Tt4$XoWD:pRM%4034r%23%40Tt4%24XoWD@cluster0.zqmge.mongodb.net/react-movie-browser-db?retryWrites=true&w=majority`, {
+mongoose.connect(`mongodb+srv://mongobagngabamamemafofofo:pRM%4034r%23%40Tt4%24XoWD@cluster0.zqmge.mongodb.net/react-movie-browser-db?retryWrites=true&w=majority`, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => console.log('polaczono z db')).catch(() => console.log('nie udalo sie polaczyc z db'))
